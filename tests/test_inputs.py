b1cd57cec07c94d6ec7c60d45e3a8c30e51930d8
Delpfi <120664976+Delpfi@users.noreply.github.com>
2023-08-01 16:25:36 +0300

    home12*

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -5,6 +5,7 @@ from selenium.webdriver import Keys
 from conftest import browser
 from pages.herokuapp_inputs import Inputs
 #Экспоненциальная запись числа
+"""До конца на 100% не доделал"""
 def test_inputs(browser):
     inputs = Inputs(browser)
     inputs.visit()
@@ -14,7 +15,30 @@ def test_inputs(browser):
     inputs.fild.send_keys(key)
     time.sleep(2)
     inputs.fild.clear()
-    key1 = "e-1232"
+    """Key_ исходная строка """
+    key1 = "-12e-e32"
+    list_key = ["E","e"]
+    list_num = ["0","1","2","3","4","5","6","7","8","9"]
+    list_key_ch = ["-"]
+    list_result = []
+    """Отфильтровать строку"""
+    e=0
+    for i in key1:
+        if i in list_key:
+            if e != 0:
+                continue
+            e+=1
+            if not i in list_result:
+                list_result.append(i)
+        elif i in list_num:  #если есть в списке list_num, то добавляем в итоговый список
+            list_result.append(i)
+        elif i in list_key_ch:
+            list_result.append(i)
+
+    """Проверить нужно исходную строчку и отфильтрованную """
+
+
+    print(list_result)
     inputs.fild.send_keys(key1)
     inputs.fild.send_keys(Keys.ENTER)
     #print(inputs.fild_1.find_element())
@@ -33,5 +57,3 @@ def test_inputs(browser):
     time.sleep(2)
     inputs.fild.clear()
     time.sleep(3)
-
-    #assert inputs.fild.text == "e-1232"
